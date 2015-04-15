6cca3f67c51a6c2730e83467b766dbb723bbbfeb
Hackbright Student <info@hackbrightacademy.com>
2015-04-14 17:56:14 -0700

    made the dictionary!

--- markov.py
+++ markov.py
@@ -1,50 +1,40 @@
-# import sys 
+import sys 
+print sys.argv
+
+green_eggs = open('green-eggs.txt')
+
+def make_chains(green_eggs):
+    """Takes input text as string; returns dictionary of markov chains."""
+mark_chains = {}
 
 
-#green_eggs = open(green_file_path)
+text_green_eggs = green_eggs.read()
+green_eggs_list = text_green_eggs.split()
 
-
-
-def word_list(green_file_path):
-    green_eggs = open(green_file_path)
+for i in range(len(green_eggs_list) - 2):
+    key = (green_eggs_list[i], green_eggs_list[i + 1])
+    value =  green_eggs_list[i + 2]
    
-    use_all_text = green_eggs.read() #instead of looping through each line 
-    string_text = use_all_text.split() #splits on all whitespace
+    if key not in mark_chains:
+        mark_chains[key] = []
+        
+    mark_chains[key].append(value)
 
-    # for line in green_eggs:
-    # #     line_data = line.rstrip().split(" ")
-    # #     file_text_list.extend(line_data)
-    
-   
-
-
-    bituple = []
-    for i in range(len(string_text)-1):
-        word_dict = {}
-        word_dict.keys([string_text[i]])
-        word_dict.values([string_text[i + 1]])
-        i += 2 
-        print word_dict
-
-word_list("green-eggs.txt")
-
-# def make_chains(green_file_path):
-#  """Takes input text as string; returns dictionary of markov chains."""
-#     bituple_list = file_text_list[0] + file_text_list[1]
-#     print bituple_list
-
-
-# make_chains("green-eggs.txt")
-
-
-#return {} #each bi gram
+print mark_chains
 
 
 
-
-
-
-
+#     #we have the words in a list separately as items and we have created an empty dict
+#         #for the length of the list, we want to:
+#         #get the [0] term, assign it to key
+#         # get the [1] term, assign it to value
+#         # get the [1] term, assign to key
+#         # get the [2] term, assign to value, etc until the end of the list
+#     # for len(green_eggs_list):
+#     #     green_eggs_list[0] = key
+#     #     green_eggs_list[1] = value
+#     #     green_eggs_dict[key] = [value]
+#     # return {}
 
 # def make_text(chains):
 #     """Takes dictionary of markov chains; returns random text."""
@@ -65,6 +55,3 @@ word_list("green-eggs.txt")
 # random_text = make_text(chain_dict)
 
 # print random_text
-
-
-
